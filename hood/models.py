100073f8d530bdda56d0648c463ade75d4108812
Natasha Kinuthia <kinuthianatasha@gmail.com>
2021-06-04 10:13:30 +0300

    updated settings

--- hood/models.py
+++ hood/models.py
@@ -10,7 +10,7 @@ class NeighbourHood(models.Model):
     name = models.CharField(max_length=50)
     location = models.CharField(max_length=60)
     admin = models.ForeignKey("Profile", on_delete=models.CASCADE, related_name='hood')
-    hood_logo = models.CloudinaryField(upload_to='images/')
+    hood_logo = CloudinaryField(upload_to='images/')
     description = models.TextField()
     health_tell = models.IntegerField(null=True, blank=True)
     police_number = models.IntegerField(null=True, blank=True)
