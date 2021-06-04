5d173405f2c2d283194a266d9f8592a46fbdc46a
Natasha Kinuthia <kinuthianatasha@gmail.com>
2021-06-04 10:14:30 +0300

    updated settings

--- hood/models.py
+++ hood/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from pyuploadcare.dj.models import ImageField
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from cloudinary.models import CloudinaryField
@@ -33,7 +32,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     name = models.CharField(max_length=80, blank=True)
     bio = models.TextField(max_length=254, blank=True)
-    profile_picture = models.CloudinaryField(upload_to='images/', default='default.png')
+    profile_picture = CloudinaryField(upload_to='images/', default='default.png')
     location = models.CharField(max_length=50, blank=True, null=True)
     neighbourhood = models.ForeignKey(NeighbourHood, on_delete=models.SET_NULL, null=True, related_name='members', blank=True)
 
