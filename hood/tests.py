bcfb4e70b108988ff8a6bc8b5c543ef95a41eb8b
Natasha Kinuthia <kinuthianatasha@gmail.com>
2021-06-04 15:34:46 +0300

    update

--- hood/tests.py
+++ hood/tests.py
@@ -40,3 +40,15 @@ class TestPost(TestCase):
         self.image_test.delete_image()
         after = Profile.objects.all()
         self.assertTrue(len(after) < 1)
+
+
+class AuthTestCase(TestCase):
+    def setUp(self):
+        self.u = User.objects.create_user('test@dom.com', 'test@dom.com', 'pass')
+        self.u.is_staff = True
+        self.u.is_superuser = True
+        self.u.is_active = True
+        self.u.save()
+
+    def testLogin(self):
+        self.client.login(username='test@dom.com', password='pass')
