aaa0888c878f9a040297467a0b45169693699f12
Natasha Kinuthia <kinuthianatasha@gmail.com>
2021-06-04 11:34:18 +0300

    working on test.py

--- hood/tests.py
+++ hood/tests.py
@@ -2,3 +2,7 @@ from django.test import TestCase
 from .models import NeighbourHood, Profile, Business, Post
 from django.contrib.auth.models import User
 
+class ProfileTest(TestCase):
+    def setUp(self):
+        self.dorcas = User(username = 'Natasha',email = 'kinuthianatasha@gmail.com')
+        self.dorcas = Profile(user = Self.dorcas,user = 1,Bio = 'tests',photo = 'test.jpg',date_craeted='dec,01.2020')
