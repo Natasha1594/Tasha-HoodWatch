563096af869e9a7aecb11e6b261dc97288944e29
Natasha Kinuthia <kinuthianatasha@gmail.com>
2021-06-04 10:03:58 +0300

    updated settings

--- hoodwatch/settings.py
+++ hoodwatch/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 import dj_database_url
 from decouple import config, Csv
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
